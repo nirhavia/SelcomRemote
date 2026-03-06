c6ad821c9269d80d48b6b268af3ab06431692725
nirhavia <niromail@hotmail.com>
2026-03-06 11:02:41 +0200

    Add app/src/main/java/com/selcom/remote/KeyCodes.java

---
+++ app/src/main/java/com/selcom/remote/KeyCodes.java
@@ -0,0 +1,34 @@
+package com.selcom.remote;
+
+public class KeyCodes {
+    public static final int KEYCODE_BACK               = 4;
+    public static final int KEYCODE_HOME               = 3;
+    public static final int KEYCODE_MENU               = 82;
+    public static final int KEYCODE_DPAD_UP            = 19;
+    public static final int KEYCODE_DPAD_DOWN          = 20;
+    public static final int KEYCODE_DPAD_LEFT          = 21;
+    public static final int KEYCODE_DPAD_RIGHT         = 22;
+    public static final int KEYCODE_DPAD_CENTER        = 23;
+    public static final int KEYCODE_VOLUME_UP          = 24;
+    public static final int KEYCODE_VOLUME_DOWN        = 25;
+    public static final int KEYCODE_VOLUME_MUTE        = 164;
+    public static final int KEYCODE_CHANNEL_UP         = 166;
+    public static final int KEYCODE_CHANNEL_DOWN       = 167;
+    public static final int KEYCODE_LAST_CHANNEL       = 207;
+    public static final int KEYCODE_INFO               = 165;
+    public static final int KEYCODE_GUIDE              = 172;
+    public static final int KEYCODE_0                  = 7;
+    public static final int KEYCODE_1                  = 8;
+    public static final int KEYCODE_2                  = 9;
+    public static final int KEYCODE_3                  = 10;
+    public static final int KEYCODE_4                  = 11;
+    public static final int KEYCODE_5                  = 12;
+    public static final int KEYCODE_6                  = 13;
+    public static final int KEYCODE_7                  = 14;
+    public static final int KEYCODE_8                  = 15;
+    public static final int KEYCODE_9                  = 16;
+    public static final int KEYCODE_MEDIA_PLAY_PAUSE   = 85;
+    public static final int KEYCODE_MEDIA_STOP         = 86;
+    public static final int KEYCODE_MEDIA_REWIND       = 89;
+    public static final int KEYCODE_MEDIA_FAST_FORWARD = 90;
+}
